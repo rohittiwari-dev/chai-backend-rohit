a65bd42590bd9a0e842e33a691a2d2935aced51e
Rohit Tiwari <tigertiwari1023@gmail.com>
2024-01-13 14:00:36 +0530

    [feature] added tweet create,fetch,update.delete endpoint

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,65 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  //TODO: create tweet
+  const errorList = [];
+  const { content, ownerId } = req.body;
+  if (!ownerId) errorList.push("ownerId");
+  if (!content) errorList.push("content");
+  if (errorList.length)
+    throw new ApiError(
+      400,
+      "Please Provide this required Fields:" + errorList.toString()
+    );
 
-const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  const createdTweet = await Tweet.create({ content, owner: ownerId });
 
-const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  res
+    .status(201)
+    .json(new ApiResponse(201, createdTweet, "Tweet Successfully Done."));
+});
 
-const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+const getUserTweets = asyncHandler(async (req, res, next) => {
+  // TODO: get user tweets
+  const userId = req.params.userId;
+  if (!userId) throw new ApiError(400, "Please Provide user id");
+  const tweetInfo = await Tweet.find({ owner: userId });
+  if (!tweetInfo) throw new ApiError(400, "No Tweet Found with this tweet id");
+  if (!tweetInfo.length)
+    throw new ApiError(400, "No Tweets Found with this user ID");
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweetInfo, "Tweets successfully fetched."));
+});
+
+const updateTweet = asyncHandler(async (req, res, next) => {
+  //TODO: update tweet
+  const tweetId = req.params.tweetId;
+  if (!tweetId) throw new ApiError(400, "Please Provide user id");
+  const tweetInfo = await Tweet.findByIdAndUpdate(tweetId);
+  if (!tweetInfo) throw new ApiError(400, "No Tweet Found with this tweet id");
+  tweetInfo.set({ ...req.body }); // Did this way because find and update function does not returns the updated data
+  await tweetInfo.save();
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweetInfo, "Tweet Successfully Updated"));
+});
+
+const deleteTweet = asyncHandler(async (req, res, next) => {
+  //TODO: update tweet
+  const tweetId = req.params.tweetId;
+  if (!tweetId) throw new ApiError(400, "Please Provide user id");
+  const tweetInfo = await Tweet.findByIdAndDelete(tweetId, { ...req.body });
+  if (!tweetInfo) throw new ApiError(400, "No Tweet Found with this tweet id");
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweetInfo, "Tweet Successfully Deleted"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
